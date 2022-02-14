49bd173bfbf8759a0d51e5c88525c6eda411613c
Makuta <nikita.o.platonov@gmail.com>
2022-02-14 20:16:24 +0200

    Added material for objects and lights

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,31 +1,43 @@
 #version 450 core
+
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 out vec4 FragColor;
 
 in vec3 FragPos;
 in vec3 Normal;
 in vec3 LightPos;
 
-uniform vec3 lightColor;
-uniform vec3 objectColor;
+uniform Light light;
+uniform Material material;
 
 void main() {
     // ambient
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = light.ambient * material.ambient;
 
     // diffuse
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(LightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
     // specular
-    float specularStrength = 1.0;
-    vec3 viewDir = normalize(-FragPos); // the viewer is always at (0,0,0) in view-space, so viewDir is (0,0,0) - Position => -Position
+    vec3 viewDir = normalize(-FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (material.specular * spec);
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
+    vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
--- src/Spheres.cpp
+++ src/Spheres.cpp
@@ -100,8 +100,15 @@ void Spheres::draw(glm::mat4 projection, glm::mat4 view, double time) {
 
     //start of sphere
     lightingShader.use();
-    lightingShader.setVec3("objectColor", 0.2f, 0.f, 0.f);
-    lightingShader.setVec3("lightColor", 1.f, 1.f, 0.2f);
+    lightingShader.setVec3("material.ambient",  1.0f, 0.5f, 0.31f);
+    lightingShader.setVec3("material.diffuse",  1.0f, 0.5f, 0.31f);
+    lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+    lightingShader.setFloat("material.shininess", 32.0f);
+
+
+    lightingShader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
+    lightingShader.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f);
+    lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
     lightingShader.setVec3("lightPos", lightPosSphere);
 
     lightingShader.setMat4("projection", projection);
